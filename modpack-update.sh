241490d6402614ada2916fd2df9523758a0a2964
V0LD3M0R7 <peukert.vit@gmail.com>
2024-11-07 14:55:37 +0100

    first imput to mod updater
    found some functuons to use in mod updater
    most important is "mapfile"
    which will create array from txt files

--- modpack-update.sh
+++ modpack-update.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+## ARMA 3 WORKSHOP DIRECTORY ##
+sourceDir="/home/vitpeukert/.local/Steam/steamapps/common/"
+## COPY OUTPUT DIRECTORY ##
+outputDir="/home/vitpeukert/LAE/modpack-update"
+## modlist
+
+# Keep the values in values.txt file, each in separate line.
+############################
+#while read value
+#do
+#    grep $value myfile.txt
+#done < values.txt
+############################
+
+# for renaming the directories use mapfile to read AddonNumberList.txt and AddonNameList.txt
+#mapfile -t myArray < file.txt
